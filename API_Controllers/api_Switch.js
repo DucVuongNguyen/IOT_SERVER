1175ac44006404bddb3d39026c6241ca6616f534
NguyenDucVuong <ducvuong.tdt@gmail.com>
2022-06-12 19:50:22 +0700

    first commit

--- API_Controllers/api_Switch.js
+++ API_Controllers/api_Switch.js
@@ -393,7 +393,7 @@ let sendData_KeySecurity = async (req, res) => {
             // console.log(result.NameDevice);
             db = Response_.Type;
             coll = Response_.NameDevice;
-            sample = { Status: Status, TimeModify: today }
+            sample = { Status: Status, TimeModify: Date(timeformat) }
             let result = await client.db(`${db}`).collection(`${coll}`).updateOne({ Date: date, Month: month, Year: year }, { $push: { samples: sample } }, { upsert: true });
             // console.log(`${result.insertedCount} new listing(s) created with the following id(s):`);
             // console.log(result.insertedIds);
