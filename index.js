6bf1fc588582d38d5f5af65e62a790bf4fd7bbdc
NguyenDucVuong <ducvuong.tdt@gmail.com>
2022-06-23 10:27:22 +0700

    first commit

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ io.on("connection", (socket) => {
                 io.sockets.in(data.room).emit('initStatus_');
             }
             case 'DeviceToApp': {
-                // io.sockets.in(data.room).emit('SyncStatus', { DataResult: data.Status, isError: 0 });
+                io.sockets.in(data.room).emit('SyncStatus', { DataResult: data.Status, isError: 0 });
                 if (Number(data.isSave) === 1) {
                     let response = await axios_Switch.sendData_KeySecurity(data.NameDevice, data.KeySecurity, data.Status);
                 }
