171792130c86891ba60d3614195ecd484181d3bc
NguyenDucVuong <ducvuong.tdt@gmail.com>
2022-06-28 01:01:09 +0700

    first commit

--- API_Controllers/api_Device.js
+++ API_Controllers/api_Device.js
@@ -159,7 +159,7 @@ let renameDevice = async (req, res) => {
     const client = new MongoClient(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
     try {
 
-        if (!req.body.UserName || !req.body.Password || !req.body.NameDevice || !req.body.RenameDevice || !req.body.Key) {
+        if (!req.body.UserName || !req.body.Password || !req.body.NameDevice || !req.body.RenameDevice) {
             return res.status(200).json({
                 message: `Thông tin không để trống!`,
                 isError: 1
@@ -168,13 +168,12 @@ let renameDevice = async (req, res) => {
         let UserName = req.body.UserName;
         let Password = req.body.Password;
         let NameDevice = req.body.NameDevice;
-        let Key = req.body.Key;
         let RenameDevice = req.body.RenameDevice;
         
-        let db = `Devices_Manager`;
-        let coll = `Devices_`;
+        let db = `ManagerAccounts`;
+        let coll = `Users`;
         await client.connect();
-        let result = await client.db(`${db}`).collection(`${coll}`).findOne({ NameDevice: NameDevice, Key: Key });
+        let result = await client.db(`${db}`).collection(`${coll}`).findOne({ UserName: UserName, Password: Password });
         let Response_ = result;
         if (Response_) {
             let DevicesArr = Response_.Devices;
