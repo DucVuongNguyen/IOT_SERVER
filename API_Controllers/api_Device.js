bae67c64cdf9f188c4562159a6add3420c2c99c3
NguyenDucVuong <ducvuong.tdt@gmail.com>
2022-06-28 00:54:34 +0700

    first commit

--- API_Controllers/api_Device.js
+++ API_Controllers/api_Device.js
@@ -159,7 +159,7 @@ let renameDevice = async (req, res) => {
     const client = new MongoClient(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
     try {
 
-        if (!req.body.UserName || !req.body.Password || !req.body.NameDevice || !req.body.RenameDevice) {
+        if (!req.body.UserName || !req.body.Password || !req.body.NameDevice || !req.body.RenameDevice || !req.body.Key) {
             return res.status(200).json({
                 message: `Thông tin không để trống!`,
                 isError: 1
@@ -168,7 +168,9 @@ let renameDevice = async (req, res) => {
         let UserName = req.body.UserName;
         let Password = req.body.Password;
         let NameDevice = req.body.NameDevice;
+        let Key = req.body.Key;
         let RenameDevice = req.body.RenameDevice;
+        
         let db = `Devices_Manager`;
         let coll = `Devices_`;
         await client.connect();
